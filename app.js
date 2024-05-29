24a8c11ef7ab047efe23b50df80b7edff65daf32
Victor <matiasvferrada@hotmail.com>
2024-05-28 21:22:05 -0400

    ultimo

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 function addProduct() {
-    const productName = document.getElementById('productName').value;
+    const producnom = document.getElementById('producnom').value;
     const productValue = parseFloat(document.getElementById('productValue').value);
     const iva = productValue * 0.19;
     const totalValue = productValue + iva;
 
-    if (productName && productValue) {
-        const table = document.getElementById('productTable').getElementsByTagName('tbody')[0];
-        const newRow = table.insertRow();
+    if (producnom && productValue) {
+        const table = document.getElementById('tablaproduc').getElementsByTagName('tbody')[0];
+        
+        const newRow = document.createElement('tr');
 
-        const nameCell = newRow.insertCell(0);
-        const valueCell = newRow.insertCell(1);
-        const ivaCell = newRow.insertCell(2);
-        const totalCell = newRow.insertCell(3);
+        const nameCell = document.createElement('td');
+        const valueCell = document.createElement('td');
+        const ivaCell = document.createElement('td');
+        const totalCell = document.createElement('td');
 
-        nameCell.innerText = productName;
-        valueCell.innerText = productValue.toFixed(2);
-        ivaCell.innerText = iva.toFixed(2);
-        totalCell.innerText = totalValue.toFixed(2);
+        nameCell.appendChild(document.createTextNode(producnom));
+        valueCell.appendChild(document.createTextNode(productValue.toFixed(2)));
+        ivaCell.appendChild(document.createTextNode(iva.toFixed(2)));
+        totalCell.appendChild(document.createTextNode(totalValue.toFixed(2)));
+
+        newRow.appendChild(nameCell);
+        newRow.appendChild(valueCell);
+        newRow.appendChild(ivaCell);
+        newRow.appendChild(totalCell);
+
+        table.appendChild(newRow);
 
         document.getElementById('productForm').reset();
     } else {
